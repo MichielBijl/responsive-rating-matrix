220e7227607e4a0b9d7cceb302ed9e594c654b93
Michiel Bijl <m.bijl@lukkien.com>
2015-12-24 02:05:42 +0100

    Add JS to table example

---
+++ table.js
@@ -0,0 +1,56 @@
+(function(){
+  var table = document.getElementById('survey');
+  var rows = table.querySelectorAll('tr');
+  var rowHeadings = table.querySelectorAll('th');
+  var cells = table.querySelectorAll('td');
+
+  adjustFormat();
+
+  window.addEventListener('resize', adjustFormat);
+
+  function adjustFormat () {
+    var isTable = window.getComputedStyle(table).getPropertyValue('display') === "table";
+
+    switch (isTable) {
+      case true:
+        turnIntoTable();
+        break;
+      case false:
+        turnIntoList();
+        break;
+      default:
+        throw new Error('You broke it');
+    }
+  }
+
+  function turnIntoTable (){
+    setRole(rows, '');
+    setRole(rowHeadings, '');
+    setAria(rowHeadings, 'level', '');
+    setRole(cells, '');
+  }
+
+  function turnIntoList() {
+    setRole(rows, 'group');
+    setRole(rowHeadings, 'heading');
+    setAria(rowHeadings, 'level', '2');
+    setRole(cells, 'presentation');
+  }
+
+  function setRole(group, role) {
+    for (var groupIndex = 0; groupIndex < group.length; groupIndex++) {
+      group[groupIndex].setAttribute('role', role);
+    }
+  }
+
+  function setAria(group, attribute, value) {
+    for (var groupIndex = 0; groupIndex < group.length; groupIndex++) {
+      group[groupIndex].setAttribute(attribute, value);
+    }
+  }
+
+  function getArray(node) {
+    // Return flattened array
+    return Array.prototype.slice.call(node.toString());
+  }
+})();
